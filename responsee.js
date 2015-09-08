4cad5d49708909c4a56f509e4b27b39c9fdbc068
Vision Design - graphic zoo <info@visiondesign.sk>
2015-09-08 15:01:16 +0200

    Update responsee.js

--- responsee.js
+++ responsee.js
@@ -1,4 +1,33 @@
-jQuery(document).ready(function() {
+/*
+ * Responsee JS - v3 - 2015-08-22
+ * http://www.myresponsee.com
+ * Copyright 2015, Vision Design - graphic zoo
+ * Free to use under the MIT license.
+*/
+jQuery(document).ready(function($) {
+  //Responsee tabs
+  $('.tabs').each(function(intex, element) {
+      current_tabs = $(this);
+      $(this).prepend('<div class="tab-nav line"></div>');
+      var tab_buttons = $(element).find('.tab-label');
+      $(this).children('.tab-nav').prepend(tab_buttons);
+      $(this).children('.tab-item').each(function(i) {
+          $(this).attr("id", "tab-" + (i + 1));
+      });
+      $(".tab-nav").each(function() {
+          $(this).children().each(function(i) {
+              $(this).attr("href", "#tab-" + (i + 1));
+          });
+      });
+      $(this).find(".tab-nav a").click(function(event) {
+          $(this).parent().children().removeClass("active-btn");
+          $(this).addClass("active-btn");
+          var tab = $(this).attr("href");
+          $(this).parent().parent().find(".tab-item").not(tab).css("display", "none");
+          $(this).parent().parent().find(tab).fadeIn();
+          return false;
+      });
+  });
   //Responsee eside nav
   $('.aside-nav > ul > li ul').each(function(index, element) {
     var count = $(element).find('li').length;
@@ -7,10 +36,14 @@ jQuery(document).ready(function() {
   });
   $('.aside-nav > ul > li:has(ul)').addClass('aside-submenu');
   $('.aside-nav > ul ul > li:has(ul)').addClass('aside-sub-submenu'); 
-    $('.aside-nav > ul > li.aside-submenu > a').attr('aria-haspopup', 'true').click(function() {  
+    $('.aside-nav > ul > li.aside-submenu > a').attr('aria-haspopup', 'true').click(function() {
+    //Close other open sub menus
+    $('.aside-nav ul li.aside-submenu > ul').removeClass('show-aside-ul', 'slow');
     $('.aside-nav ul li.aside-submenu:hover > ul').toggleClass('show-aside-ul', 'slow'); 
   }); 
   $('.aside-nav > ul ul > li.aside-sub-submenu > a').attr('aria-haspopup', 'true').click(function() { 
+    //Close other open sub menus
+    $('.aside-nav ul ul li > ul').removeClass('show-aside-ul', 'slow');
     $('.aside-nav ul ul li:hover > ul').toggleClass('show-aside-ul', 'slow');
   });
   //Responsee nav   
@@ -19,15 +52,16 @@ jQuery(document).ready(function() {
     var content = '<span class="count-number"> ' + count + '</span>';
     $(element).closest('li').children('a').append(content);
   });
-  $('.top-nav > ul li:has(ul)')
-  .addClass('submenu');
-  $('.top-nav > ul ul li:has(ul)')
-  .addClass('sub-submenu')
-  .removeClass('submenu');
-  $('.top-nav > ul li.submenu > a').click(function() {  
-    $('.top-nav > ul li.submenu:hover > ul').toggleClass('show-ul', 'slow'); 
+  $('.top-nav > ul li:has(ul)').addClass('submenu');
+  $('.top-nav > ul ul li:has(ul)').addClass('sub-submenu').removeClass('submenu');
+  $('.top-nav > ul li.submenu > a').attr('aria-haspopup', 'true').click(function() { 
+    //Close other open sub menus
+    $('.top-nav > ul li.submenu > ul').removeClass('show-ul', 'slow'); 
+    $('.top-nav > ul li.submenu:hover > ul').toggleClass('show-ul', 'slow');
   }); 
-  $('.top-nav > ul ul > li.sub-submenu > a').click(function() { 
+  $('.top-nav > ul ul > li.sub-submenu > a').attr('aria-haspopup', 'true').click(function() {  
+    //Close other open sub menus
+    $('.top-nav ul ul li > ul').removeClass('show-ul', 'slow');  
     $('.top-nav ul ul li:hover > ul').toggleClass('show-ul', 'slow');   
   });
   $('.nav-text').click(function() { 
@@ -68,13 +102,16 @@ jQuery(document).ready(function() {
   $('a').filter(function() {
     return this.href == url;
   }).parent('li').addClass('active-item');
+  var url = window.location.href;
   $('.aside-nav a').filter(function() {
     return this.href == url;
-  }).parent('li').parent('ul').addClass('active-aside-item')
-      .filter(function() {
-        return this.href == url;
-      }).parent('li').parent('ul').parent('li').parent('ul').addClass('active-aside-item')
-      .filter(function() {
-        return this.href == url;
-      }).parent('li').parent('ul').parent('li').parent('ul').parent('li').parent('ul').addClass('active-aside-item');
+  }).parent('li').parent('ul').addClass('active-aside-item');
+  var url = window.location.href;
+  $('.aside-nav a').filter(function() {
+    return this.href == url;
+  }).parent('li').parent('ul').parent('li').parent('ul').addClass('active-aside-item');
+  var url = window.location.href;
+  $('.aside-nav a').filter(function() {
+    return this.href == url;
+  }).parent('li').parent('ul').parent('li').parent('ul').parent('li').parent('ul').addClass('active-aside-item');
 });
